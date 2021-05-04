6dbe6dd4a6461cfb0c5e6af64eb04ad8beaeb07a
Josh Weidenaar <josh.weidenaar@intersectillinois.org>
2021-05-04 00:00:01 -0500

    Diagram added

---
+++ EmployeeSQL/QuickDBD-Free Diagram.sql
@@ -0,0 +1,85 @@
+﻿
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/MlFC0K
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+drop table "Employee" CASCADE;
+drop table "Dept_manager" CASCADE;
+drop table "Departments" CASCADE;
+drop table "Dept_emp" CASCADE;
+drop table "Salaries" CASCADE;
+drop table "Titles" CASCADE;
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/MlFC0K
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Employee" (
+    "emp_no" varchar   NOT NULL,
+    "emp_title_id" varchar   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" varchar(255)   NULL,
+    "last_name" varchar(255)   NULL,
+    "sex" char(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" varchar   NOT NULL,
+    "title" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" varchar   NOT NULL
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" varchar   NOT NULL,
+    "dept_no" varchar   NOT NULL
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" varchar   NOT NULL,
+    "salary" money   NOT NULL
+);
+
+ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
